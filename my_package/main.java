70c083bdf0d7da52ef2bc0aa86f037e25e97af99
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-01 23:20:46 +0530

    about encapsulation

---
+++ my_package/main.java
@@ -0,0 +1,8 @@
+package my_package;
+import java.util.*;
+class main{
+public static void main(String agrs[]){
+person p=new person("Deepak",22);
+}
+
+}
---
+++ my_package/person.java
@@ -0,0 +1,14 @@
+package my_package;
+import java.util.Scanner.*;
+class person{
+private String name;
+private int age;
+ person(String name,int age){
+this.name=name;
+this.age=age;
+System.out.println(name+" "+age);
+}
+}
+
+
+
