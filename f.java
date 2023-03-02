79754c95df13c196e3f9719b4864786027044644
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-02 09:48:49 +0530

    use of final keyword in java

---
+++ f.java
@@ -0,0 +1,11 @@
+public class f {
+    final int speedlimit=90;//final variable  
+    void run(){  
+         speedlimit=9;
+     System.out.println(speedlimit);  
+    }  
+    public static void main(String args[]){  
+    f obj=new f();  
+    obj.run();  
+}
+}
