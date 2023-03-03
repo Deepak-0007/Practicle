2a66c52b3ff25a3d7a56a329b2b033b74d56647a
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-03 21:42:57 +0530

    print array in 2 method

---
+++ array2.java
@@ -0,0 +1,16 @@
+package array;
+
+public class array2 {
+public static void main(String[] args) {
+	int a[]=new int [4];
+	a[0]=2;
+	a[1]=3;
+	a[2]=4;
+	a[3]=5;
+	// travsering array each one
+	for(int i:a)
+//	for(int i=0;i<a.length;i++) 
+		System.out.println(i);
+	
+}
+}
