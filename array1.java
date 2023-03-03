42101929337553e76b36bce61593c3572e8a777b
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-03 21:40:44 +0530

    print array with basic term

---
+++ array1.java
@@ -0,0 +1,15 @@
+package array;
+
+public class array1 {
+public static void main(String[] args) {
+	String [] name={"Deepak","Harinder","vikash","Dheeraj"};
+	for(int i=0;i<name.length;i++) {
+		System.out.println(name[i]);
+	}
+	System.out.println();
+	int [] num= {1,2,3,4,5};
+	for(int j=0;j<num.length;j++) {
+		System.out.println(num[j]);
+	}
+}
+}
