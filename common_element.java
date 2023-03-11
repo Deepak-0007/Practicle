3592a20527f0b70fcb7e4df6d21d5ee1ffb26310
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-11 23:17:11 +0530

    sort the array with in built function

---
+++ common_element.java
@@ -0,0 +1,20 @@
+package lab;
+
+
+	import java.io.*;
+	import java.util.*;
+	public class common_element {
+	public static void main(String[] args) {
+		int arr[]= {2,9,4,5,1};
+		for(int i=0;i<arr.length;i++) {
+		Arrays.sort(arr);
+		}
+		for(int i=0;i<arr.length;i++)
+		
+		System.out.println(arr[i]);
+	}}
+	
+
+
+
+ 
