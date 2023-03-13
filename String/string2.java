549e4cfe71d14d938099fab78bd1b22576e962b0
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-13 16:08:05 +0530

    Update string2.java

--- String/string2.java
+++ String/string2.java
@@ -5,12 +5,12 @@ public class string2 {
 
 		char[] arr={'d','e','e','p','k'};
 		System.out.println(arr);
-		String s=new String(arr);
+		String s=new String(arr); // by using new keyword creating string obj
 		System.out.println("value is  "+s);//"deepk
 		}
 	public void myfun() {//method 2
 		String s1="deepak";
-		String s2=new String(s1);
+		String s2=new String(s1);// by using literal
 		System.out.println(s2);//deepak
 	}
 		
