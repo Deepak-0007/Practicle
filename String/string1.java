a30ab2a2ecd00d49db84110ed46f79339484d392
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-13 15:59:14 +0530

    to print String

---
+++ String/string1.java
@@ -0,0 +1,21 @@
+package String;
+
+public class string1 {
+	public void show() {
+
+	char[] arr={'d','e','e','p','k'};
+	System.out.println(arr);
+	String s=new String(arr);
+	System.out.println("value is  "+s);
+	}
+	
+	public static void main(String[] args) {
+		string1 str=new string1();
+	str.show();
+	
+	}
+	
+	}
+
+
+
