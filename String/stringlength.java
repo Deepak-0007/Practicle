6d10d040a3ba9750a31a16cf32090ee7b30efab3
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-14 10:36:25 +0530

    to find the length of the String

---
+++ String/stringlength.java
@@ -0,0 +1,10 @@
+package String;
+
+public class stringlength {
+	public static void main(String[] args) {
+		String s="anudip foundation";
+				
+				System.out.println("the string length of "+s+" " +s.length() );
+	}
+
+}
