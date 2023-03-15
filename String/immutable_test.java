44d8bb42c3aad8e466ec7b235d95c886b9a6632f
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-15 22:15:13 +0530

    checking string is mutable or not

---
+++ String/immutable_test.java
@@ -0,0 +1,9 @@
+package String;
+
+public class immutable_test {
+public static void main(String[] args) {
+	String str="ajay";
+	str.concat("yadav");// insert the string at the end
+	System.out.println(str); // it will only display ajay
+}
+}
