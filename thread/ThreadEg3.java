a0b9df1f5a6f7b732539cd075521770c3480745f
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-03-27 21:37:21 +0530

    using of thread with try and catch

---
+++ thread/ThreadEg3.java
@@ -0,0 +1,37 @@
+class ThreadEg3 extends Thread {
+    public void run(){
+        for(int i=0; i<5; i++){
+            try{
+                Thread.sleep(180);
+                }
+                catch(InterruptedException e)
+                {
+                System.out.println(e);
+                }
+                System.out.println(i);    
+        }
+    }
+     public static void main(String[] args) {
+            ThreadEg3 t=new ThreadEg3();
+            ThreadEg3 t1=new ThreadEg3();
+            ThreadEg3 t2=new ThreadEg3();
+            t.start();
+            try{
+                t.join();
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println(e);
+            }
+            t1.start();
+            try{
+                t1.join();
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println(e);
+            }
+            t2.start();
+           
+        } 
+}
