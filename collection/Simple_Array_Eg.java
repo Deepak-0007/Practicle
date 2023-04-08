5cf200813afb53ac8987088d956e627d4adbd3e7
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-04-08 22:48:04 +0530

    to print simple array in array List

---
+++ collection/Simple_Array_Eg.java
@@ -0,0 +1,13 @@
+package collection;
+import java.util.ArrayList;
+public class Simple_Array_Eg {
+public static void main(String[] args) {
+	ArrayList<String>a1=new ArrayList<String>();
+	// add element in arrayList 
+	a1.add("hello");
+	a1.add("java");
+	a1.add("world ");
+	// printing arraylist
+	System.out.println(a1);
+}
+}
